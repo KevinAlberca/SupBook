4fb0ae8396bc4f716bdd0488d85733fa31356d14
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-03-05 18:40:22 +0100

    Content entity created

---
+++ src/AppBundle/Entity/Content.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Content
+ *
+ * @ORM\Table(name="content")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ContentRepository")
+ */
+class Content
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="post_date", type="datetime")
+     */
+    private $postDate;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_author", type="integer")
+     */
+    private $idAuthor;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set postDate
+     *
+     * @param \DateTime $postDate
+     *
+     * @return Content
+     */
+    public function setPostDate($postDate)
+    {
+        $this->postDate = $postDate;
+
+        return $this;
+    }
+
+    /**
+     * Get postDate
+     *
+     * @return \DateTime
+     */
+    public function getPostDate()
+    {
+        return $this->postDate;
+    }
+
+    /**
+     * Set idAuthor
+     *
+     * @param integer $idAuthor
+     *
+     * @return Content
+     */
+    public function setIdAuthor($idAuthor)
+    {
+        $this->idAuthor = $idAuthor;
+
+        return $this;
+    }
+
+    /**
+     * Get idAuthor
+     *
+     * @return int
+     */
+    public function getIdAuthor()
+    {
+        return $this->idAuthor;
+    }
+}
+
---
+++ src/AppBundle/Repository/ContentRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: AwH
+ * Date: 05/03/16
+ * Time: 17:41
+ */
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class ContentRepository extends EntityRepository
+{
+
+}
