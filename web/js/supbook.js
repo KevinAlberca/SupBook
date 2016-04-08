dcbee0f98912a0b1bfa3202a51eefadfe892f3dc
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-04-08 22:33:35 +0200

    New webdesign : Material design

--- web/js/supbook.js
+++ web/js/supbook.js
@@ -1,21 +1,4 @@
 $(function(){
+    $(".button-collapse").sideNav();
 
-    var sidebar = $('#sidebar');
-    var container = $('.container');
-
-    $('#toggle-sidebar').click(function(){
-        if(sidebar.hasClass('expanded')){
-            sidebar.removeClass('expanded', 400);
-            //container.css({
-            //  "margin-left": "auto",
-            // "margin-right": "auto"
-            // });
-        } else {
-            sidebar.addClass('expanded', 400);
-            //container.css({
-            //  "margin-left": "auto",
-            //  "margin-right": "auto"
-            // });
-        }
-    });
 });
