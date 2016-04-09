cd34cd7dc28f44fa2dbbb02800f2f957421b15d5
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-04-09 15:32:54 +0200

    Show comments per threads

--- web/js/supbook.js
+++ web/js/supbook.js
@@ -1,4 +1,16 @@
 $(function(){
     $(".button-collapse").sideNav();
 
+    $(".reply-to").click(function(){
+        console.log($(this).val());
+
+        $(".comment").css({
+            "display":"none"
+        });
+
+        $(".comments-"+$(this).val()).css({
+            "display":"block"
+        });
+
+    })
 });
